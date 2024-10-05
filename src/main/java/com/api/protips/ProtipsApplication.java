0d4f6c63d1e8374d6012beea2048cee74db9911e
GuiAmerico <guilhermao127@gmail.com>
2024-10-05 09:43:01 -0300

    :tada: init

---
+++ src/main/java/com/api/protips/ProtipsApplication.java
@@ -0,0 +1,13 @@
+package com.api.protips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtipsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProtipsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/api/protips/ProtipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.protips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtipsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
